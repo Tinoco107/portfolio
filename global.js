623f5bb9e46ab69d99424b4aed236539cee7d533
Angel Tinoco <atinoco@ucsd.edu>
2025-04-18 17:58:55 -0700

    lab 3 step 2 atmpt 1

--- global.js
+++ global.js
@@ -3,3 +3,15 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+// Step 2: Automatic current page link
+// Get an array of all nav links
+let navLinks = $$("nav a");
+
+// Find the link whose host and pathname match the current page
+let currentLink = navLinks.find(
+  (a) => a.host === location.host && a.pathname === location.pathname
+);
+
+// If a link is found, add the 'current' class
+currentLink?.classList.add('current');
