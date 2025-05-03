1a6af970d354fac0f8f84a3dfa3cde5cdb2ed772
Angel Tinoco <atinoco@ucsd.edu>
2025-05-02 23:12:00 -0700

    more bug fixing

--- projects/projects.js
+++ projects/projects.js
@@ -1,7 +1,7 @@
 // Import D3 from the CDN
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 // Import global functions using the correct relative path
-import { fetchJSON, renderProjects } from '/global.js';
+import { fetchJSON, renderProjects } from '../global.js';
 
 function loadProjectsData() {
   fetchJSON('lib/projects.json')
